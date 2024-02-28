21fe5a820bd8cb0f91ccd361ee4cfea8817fcd5a
UniBreakfast <unibreakfast1@gmail.com>
2024-02-28 13:50:58 +0200

    fix price

--- src/app/core/interfaces/catalog.ts
+++ src/app/core/interfaces/catalog.ts
@@ -9,7 +9,8 @@ export interface CatalogCard {
     level: string,
     rating: number,
     views: number,    
-    reviews: []
+    reviews: [],
+    price: number,
 }
 
 export interface CatalogCards {
