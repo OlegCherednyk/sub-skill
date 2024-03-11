db0aa4b57dcb742cb5481da24e2679ea19c58af0
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2024-03-11 13:26:14 +0100

    feat: some styles update

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { SearchComponent } from './search/search.component';
 import { CartComponent } from './cart/cart.component';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, SearchComponent, CartComponent],
+  imports: [CommonModule, SearchComponent, CartComponent, LoginComponent],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
---
+++ src/app/layout/header/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent {
+  openLoginPage() {
+    console.log('You are opening Login page');
+  }
+}
