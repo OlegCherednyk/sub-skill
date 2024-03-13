8afffa7073e02e11872971c8c8c292994e5a2fe7
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2024-03-13 13:05:28 +0100

    feat: add search

--- src/app/core/services/catalog-card.service.ts
+++ src/app/core/services/catalog-card.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map, shareReplay, tap } from 'rxjs/operators';
 import { base_url } from '../core_variables';
 import { CatalogCard, CatalogCardsData } from '../interfaces/catalog';
 
@@ -8,9 +9,14 @@ import { CatalogCard, CatalogCardsData } from '../interfaces/catalog';
   providedIn: 'root',
 })
 export class CatalogCardService {
-  constructor(private _httpClient: HttpClient) {}
+  private cachedCatalogCards$: Observable<CatalogCard[]>;
 
-  public getCatalogCardsData(): Observable<CatalogCard[]> {
+  constructor(private _httpClient: HttpClient) {
+    this.cachedCatalogCards$ = this.fetchCatalogCards();
+  }
+
+  private fetchCatalogCards(): Observable<CatalogCard[]> {
+    console.log('Fetching data from server');
     return this._httpClient
       .get<CatalogCardsData>(`${base_url}microskill/all-paging`)
       .pipe(
@@ -21,7 +27,15 @@ export class CatalogCardService {
             ...card,
             photo: `data:image/jpeg;base64,${card.photo}`,
           }))
-        )
+        ),
+        tap(cards => {
+          console.log('Fetched data and stored in cache:', cards);
+        }),
+        shareReplay(1)
       );
+  }
+
+  public getCatalogCardsData(): Observable<CatalogCard[]> {
+    return this.cachedCatalogCards$;
   }
 }
